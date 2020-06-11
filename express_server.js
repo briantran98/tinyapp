c2b6a3170fa8a4a7b5ba7637ace8c64bd8b78247
Brian Tran <98brian.tran@gmail.com>
2020-06-11 20:40:23 +0000

    Changed password from plain text to hashed

--- express_server.js
+++ express_server.js
@@ -4,14 +4,16 @@ const PORT = 8080;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { v4: uuid } = require('uuid');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'b2xVn2': { longURL: 'lighthouselabs.ca', userID: 'userRandomID' },
-  '9sm5xK': { longURL: 'http://www.google.com',  userID: 'user2RandomID'}
+  'b2xVn2': { longURL: 'http://lighthouselabs.ca', userID: 'userRandomID' },
+  '9sm5xK': { longURL: 'http://www.google.com', userID: 'user2RandomID' }
 };
 
 const users = {
@@ -28,16 +30,16 @@ const users = {
 };
 
 app.get('/login', (req, res) => {
-  const id = req.cookies.user_id
-  const user = users[id]
-  const templateVars = {user};
+  const id = req.cookies.user_id;
+  const user = users[id];
+  const templateVars = { user };
   return res.render('login', templateVars);
 });
 
 app.get('/register', (req, res) => {
-  const id = req.cookies.user_id
-  const user = users[id]
-  const templateVars = {user};
+  const id = req.cookies.user_id;
+  const user = users[id];
+  const templateVars = { user };
   return res.render('register', templateVars);
 });
 
@@ -46,32 +48,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const id = req.cookies.user_id
-  const user = users[id]
+  const id = req.cookies.user_id;
+  const user = users[id];
   const validURL = urlsForUser(id);
-  const templateVars = {urls: validURL, user};
+  const templateVars = { urls: validURL, user };
   return res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  const id = req.cookies.user_id
-  const user = users[id]
-  const templateVars = {user};
+  const id = req.cookies.user_id;
+  const user = users[id];
+  const templateVars = { user };
   if (!templateVars.user) {
-    return res.redirect('/login')
+    return res.redirect('/login');
   }
   return res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const id = req.cookies.user_id
+  const id = req.cookies.user_id;
   const user = users[id];
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  const validUser = id === urlDatabase[req.params.shortURL].userID? true : false;
-  const templateVars = { shortURL: req.params.shortURL, longURL, user, validUser};
+  const validUser = id === urlDatabase[req.params.shortURL].userID ? true : false;
+  const templateVars = { shortURL: req.params.shortURL, longURL, user, validUser };
   return res.render('urls_show', templateVars);
 });
-
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
@@ -88,8 +89,7 @@ app.get('/hello', (req, res) => {
 
 app.post('/register', (req, res) => {
   const id = uuid().split('-')[0];
-  const email = req.body.email;
-  const password = req.body.password;
+  const { email, password } = req.body;
   if (!email || !password) {
     return res.sendStatus(400);
   }
@@ -97,19 +97,27 @@ app.post('/register', (req, res) => {
   if (validateLogin(email)) {
     return res.sendStatus(400);
   }
-  
-  const newUser = {
-    email,
-    password,
-    id: id
-  };
-  users[id] = newUser;
-  res.cookie('user_id', id);
-  return res.redirect('/urls');
+
+  bcrypt.hash(req.body.password, saltRounds, (err, hashPassword) => {
+    // Store hash in your password DB.
+    if (err) {
+      throw Error('Error occured when registering ', err);
+    }
+
+    const newUser = {
+      email,
+      password: hashPassword,
+      id: id
+    };
+    users[id] = newUser;
+    res.cookie('user_id', id);
+    console.log(users);
+    return res.redirect('/urls');
+  });
 });
 
 app.post('/urls', (req, res) => {
-  const shortURL = uuid().split('-')[0];
+  const shortURL = uuid().split('-')[0].slice(0, 6);
   const longURL = req.body.longURL;
   const userID = req.cookies.user_id;
   urlDatabase[shortURL] = { longURL, userID };
@@ -117,16 +125,24 @@ app.post('/urls', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const user = (validateLogin(email, password));
-
-  if (!user) {
-    return res.sendStatus(403);
-  }
+  const { email, password } = req.body;
   
-  res.cookie('user_id', user.id);
-  return res.redirect('/urls/');
+  // const email = req.body.email;
+  // const password = req.body.password;
+  // const user = (validateLogin(email, password));
+
+  for (const user in users) {
+    if (users[user].email === email) {
+      bcrypt.compare(password, users[user].password, (err, result) => {
+        if (result) {
+          res.cookie('user_id', users[user].id);
+          return res.redirect('/urls/');
+        } else {
+          return res.sendStatus(403);
+        }
+      })
+    }
+  }
 });
 
 app.post('/logout', (req, res) => {
@@ -134,14 +150,14 @@ app.post('/logout', (req, res) => {
   return res.redirect('/urls/');
 });
 
-app.post('/urls/:id' , (req, res) => {
+app.post('/urls/:id', (req, res) => {
   const id = req.cookies.user_id;
   const databaseID = urlDatabase[req.params.id].userID;
   const shortURL = req.params.id;
   const userID = req.cookies.user_id;
   const newURL = req.body.newURL;
   if (validUser(id, databaseID)) {
-    urlDatabase[shortURL] = { longURL: newURL, userID};
+    urlDatabase[shortURL] = { longURL: newURL, userID };
   }
   return res.redirect('/urls');
 });
@@ -153,13 +169,12 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   if (validUser(id, databaseID)) {
     delete urlDatabase[shortURL];
   }
-  console.log(urlDatabase)
   return res.redirect('/urls');
 });
 
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
 
 // Checks if user is in the database with email
@@ -181,7 +196,7 @@ const validateLogin = (email, password) => {
 const urlsForUser = (id) => {
   const validURL = {};
   for (const url in urlDatabase) {
-    if (urlDatabase[url].userID  === id) {
+    if (urlDatabase[url].userID === id) {
       validURL[url] = urlDatabase[url].longURL;
     }
   }
@@ -190,4 +205,4 @@ const urlsForUser = (id) => {
 
 const validUser = (currentID, posterID) => {
   return currentID === posterID;
-}
+};
