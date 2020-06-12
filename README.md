5934b5cfc00cdf90cfae4dc12a8690fd3d691329
Brian Tran <98brian.tran@gmail.com>
2020-06-12 02:11:00 +0000

    Linted and updated readme

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ TinyApp is a full stack web application built with Node and Express that allows 
 
 ## Final Product
 
-
+!["screenshot description"](https://github.com/briantran98/tinyapp/blob/master/docs/tinyapp-mainscreen.png)
+!["screenshot description"](https://github.com/briantran98/tinyapp/blob/master/docs/tinyapp-register.png)
+!["screenshot description"](https://github.com/briantran98/tinyapp/blob/master/docs/tinyapp-url-edit.png)
 
 ## Dependencies
 
--- express_server.js
+++ express_server.js
@@ -13,23 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ name: 'session', keys : ['dk233a6fnma1lm538wlz', 'kaskcn1k3jsal4n1l']}))
 app.set('view engine', 'ejs');
 
-const urlDatabase = {
-  'b2xVn2': { longURL: 'http://lighthouselabs.ca', userID: 'userRandomID' },
-  '9sm5xK': { longURL: 'http://www.google.com', userID: 'user2RandomID' }
-};
+const urlDatabase = {};
 
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
+const users = {};
 
 // GET ROUTES
 app.get('/login', (req, res) => {
